89c1383e18bc26a5556bd78d2fa291233f7a93fb
TheClonerx <theclonerx@outlook.com>
2018-12-06 18:45:20 -0300

    Fixed _ws.py for python 2

--- _ws.py
+++ _ws.py
@@ -3,9 +3,10 @@
 
 import collections
 import os
-
 import hashlib
 import base64
+import sys
+import struct
 
 CONTINUATION = 0
 TEXT = 1
@@ -27,6 +28,7 @@ def check_frame(buff):
     returns False if the buffer doesn't starts with a valid frame
     returns the size of the frame in success
     """
+    buff = bytearray(buff)
     if len(buff) < 2:
         return False
 
@@ -44,12 +46,12 @@ def check_frame(buff):
         min_size += 2
         if len(buff) < 4:
             return False
-        payload_length += int.from_bytes(buff[2:4], "big")
+        payload_length += struct.unpack_from(">H", buff, 2)[0]
     elif (buff[1] & 127) == 127:
         min_size += 8
         if len(buff) < 10:
             return False
-        payload_length += int.from_bytes(buff[2:10], "big")
+        payload_length += struct.unpack_from(">Q", buff, 2)[0]
     if len(buff) < (min_size + payload_length):
         return False
     return min_size + payload_length
@@ -59,6 +61,7 @@ def frame_info(buff):
     """
     returns a tuple that describes a frame
     """
+    buff = bytearray(buff)
     r = check_frame(buff)
     if not r:
         raise ValueError("buff is not a valid frame")
@@ -66,9 +69,9 @@ def frame_info(buff):
     if (buff[1] & 127) <= 125:
         payload_length = buff[1] & 127
     elif (buff[1] & 127) == 126:
-        payload_length += int.from_bytes(buff[2:4], "big")
+        payload_length += struct.unpack_from(">H", buff, 2)[0]
     elif (buff[1] & 127) == 127:
-        payload_length += int.from_bytes(buff[2:10], "big")
+        payload_length += struct.unpack_from(">Q", buff, 2)[0]
 
     return FrameInfo(
         bool(buff[0] & 128),
@@ -84,8 +87,6 @@ def get_frames(buff):
     begin = 0
     end = check_frame(buff)
     while end:
-        if end != len(buff):
-            print(end, len(buff))
         frames.append(buff[begin:end])
         begin = end
         end = check_frame(buff[end:])
@@ -97,7 +98,7 @@ def check_msg(buff):
     """
     returns True  if the buffer starts with a full fragmented message, or a
     unfragmented frame
-    returns where the las frame ends
+    returns where the last frame ends
 
     """
     r = check_frame(buff)
@@ -114,16 +115,18 @@ def mask_buff(buff):
     masks buff with a random mask
     retunrs mask + masked buff
     """
-    mask = os.urandom(4)
-    return mask + bytes(x ^ mask[i % 4] for i, x in enumerate(buff))
+    buff = bytearray(buff)
+    mask = bytearray(os.urandom(4))
+    return bytes(mask + bytearray(x ^ mask[i % 4] for i, x in enumerate(buff)))
 
 
 def unmask_buff(buff):
     """
     unmask buff, using the firsts 4 bytes as the mask
     """
+    buff = bytearray(buff)
     mask = buff[:4]
-    return bytes(x ^ mask[i % 4] for i, x in enumerate(buff[4:]))
+    return bytes(bytearray(x ^ mask[i % 4] for i, x in enumerate(buff[4:])))
 
 
 def encode_frame(fin=True, opcode=TEXT, mask=False, payload=None):
@@ -134,7 +137,9 @@ def encode_frame(fin=True, opcode=TEXT, mask=False, payload=None):
 
     if isinstance(payload, str):
         payload = payload.encode("utf-8", "replace")
-    elif payload is None:
+    if sys.version_info[0] < 3 and isinstance(payload, unicode):
+        paylod = payload.encode("utf-8", "replace")
+    if payload is None:
         payload = b""
     elif not isinstance(payload, (bytes, bytearray)):
         raise ValueError(
@@ -151,23 +156,22 @@ def encode_frame(fin=True, opcode=TEXT, mask=False, payload=None):
             frame.append(len(payload) | 128)
         elif len(payload) <= 65535:
             frame.append(126 | 128)
-            frame += len(payload).to_bytes(2, "big")
+            frame += struct.pack(">H", len(payload))
         else:
             frame.append(127 | 128)
-            frame += len(payload).to_bytes(8, "big")
+            frame += struct.pack(">Q", len(payload))
     else:
         if len(payload) <= 125:
             frame.append(len(payload))
         elif len(payload) <= 65535:
             frame.append(126)
-            frame += len(payload).to_bytes(2, "big")
+            frame += struct.pack(">H", len(payload))
         else:
             frame.append(127)
-            frame += len(payload).to_bytes(8, "big")
+            frame += struct.pack(">Q", len(payload))
 
     if mask:
-        frame += mask
-        frame += bytes(x ^ mask[i % 4] for i, x in enumerate(payload))
+        frame += mask_buff(payload)
     else:
         frame += payload
 
@@ -186,16 +190,26 @@ def get_payload(buff):
     info = frame_info(buff)
     if not info.payload_length:
         payload = b""
-    elif info[2]:  # if masked
-        payload = unmask_buff(buff[-(info[3] + 4):])
-    else:
-        payload = buff[-(info[3]):]
 
-    if info[1] == TEXT and info.fin:
+    if info.payload_length <= 125:
+        start_payload = 2
+    elif info.payload_length <= 65535:
+        start_payload = 4
+    else:
+        start_payload = 10
+
+    if info.masked:
+        payload = unmask_buff(
+            buff[start_payload:info.payload_length + start_payload + 4]
+        )
+    else:
+        payload = buff[start_payload:info.payload_length + start_payload]
+
+    if info.opcode == TEXT and info.fin:
         return payload.decode("utf-8", "replace")
-    elif info[1] == CLOSE:
+    elif info.opcode == CLOSE:
         return (
-            int.from_bytes(payload[:2], "big"),
+            struct.unpack_from(">H", buff)[0],
             payload[2:].decode("utf-8", "replace")
         )
 
@@ -213,6 +227,8 @@ def check_headers(headers):
         if b"\r\n\r\n" in headers:
             headers, _ = headers.split(b"\r\n\r\n", 1)
         headers = headers.decode()
+    if sys.version_info[0] < 3 and isinstance(headers, unicode):
+        headers = str(headers)
     if isinstance(headers, str):
         headers = headers.splitlines()
     if isinstance(headers, list):
@@ -226,16 +242,11 @@ def check_headers(headers):
     if version:
         version = version.split("/")[1]
         version = tuple(int(x) for x in version.split("."))
-        if version[0] < 1:
+        if version < (1, 1):
             return False
-        if version[1] < 1:
-            return False
-    if "upgrade" not in headers or headers["upgrade"] != "websocket":
+    if headers.get("upgrade", "").lower() != "websocket":
         return False
-    elif ("connection" not in headers or
-            headers["connection"].lower() != "upgrade"):
-        return False
-    elif "sec-websocket-accept" not in headers:
+    elif headers.get("connection", "").lower() != "upgrade":
         return False
 
-    return headers["sec-websocket-accept"]
+    return headers.get("sec-websocket-accept", False)
